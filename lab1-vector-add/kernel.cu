d9e33772848cf7fb4dd04c06dad24a9187b7844b
Keith Schubert <keith_schubert@baylor.edu>
2018-08-24 17:10:59 -0500

    Update kernel.cu

--- lab1-vector-add/kernel.cu
+++ lab1-vector-add/kernel.cu
@@ -11,14 +11,12 @@ __global__ void vecAddKernel(float* A, float* B, float* C, int n) {
     // Calculate global thread index based on the block and thread indices ----
 
     //INSERT KERNEL CODE HERE
-    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    int i = 
 
     // Use global index to determine which elements to read, add, and write ---
 
     //INSERT KERNEL CODE HERE
-    if (i < n) {
-        C[i] = A[i] + B[i];
-    }
+    
 
 }
 
