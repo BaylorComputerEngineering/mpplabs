a8f317cca9e90f38240b7eb7ad19277d85d7078c
Keith Schubert <keith_schubert@baylor.edu>
2018-09-10 12:55:16 -0500

    lab 2
    student files to modify

---
+++ lab2-sgemm/kernel.cu
@@ -0,0 +1,63 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+#include <stdio.h>
+
+__global__ void mysgemm(int m, int n, int k, const float *A, const float *B, float* C) {
+
+    /********************************************************************
+     *
+     * Compute C = A x B
+     *   where A is a (m x k) matrix
+     *   where B is a (k x n) matrix
+     *   where C is a (m x n) matrix
+     *
+     ********************************************************************/
+
+    // INSERT KERNEL CODE HERE
+
+
+
+}
+
+void basicSgemm(char transa, char transb, int m, int n, int k, float alpha, const float *A, int lda, const float *B, int ldb, float beta, float *C, int ldc)
+{
+    if ((transa != 'N') && (transa != 'n')) {
+	printf("unsupported value of 'transa'\n");
+    	return;
+    }
+
+    if ((transb != 'N') && (transb != 'n')) {
+	printf("unsupported value of 'transb'\n");
+	return;
+    }
+
+    if ((alpha - 1.0f > 1e-10) || (alpha - 1.0f < -1e-10)) {
+	printf("unsupported value of alpha\n");
+	return;
+    }
+
+    if ((beta - 0.0f > 1e-10) || (beta - 0.0f < -1e-10)) {
+	printf("unsupported value of beta\n");
+	return;
+    }
+
+    // Initialize thread block and kernel grid dimensions ---------------------
+
+    const unsigned int BLOCK_SIZE = 16; // Use 16x16 thread blocks
+
+    //INSERT CODE HERE
+    
+
+    // Invoke CUDA kernel -----------------------------------------------------
+
+    //INSERT CODE HERE
+
+
+
+}
