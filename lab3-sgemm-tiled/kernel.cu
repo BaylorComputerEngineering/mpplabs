2f0e3fa3c0197e49f63b7490c0eb89ff27919f91
Keith Schubert <keith_schubert@baylor.edu>
2018-09-19 12:03:31 -0500

    Lab 3
    initial commit for lab 3

---
+++ lab3-sgemm-tiled/kernel.cu
@@ -0,0 +1,67 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+#include <stdio.h>
+
+#define TILE_SZ 16
+
+__global__ void mysgemm(int m, int n, int k, const float *A, const float *B, float* C) {
+
+    /********************************************************************
+     *
+     * Compute C = A x B
+     *   where A is a (m x k) matrix
+     *   where B is a (k x n) matrix
+     *   where C is a (m x n) matrix
+     *
+     * Use shared memory for tiling
+     *
+     ********************************************************************/
+
+    // INSERT KERNEL CODE HERE
+
+
+
+
+
+}
+
+void basicSgemm(char transa, char transb, int m, int n, int k, float alpha, const float *A, int lda, const float *B, int ldb, float beta, float *C, int ldc)
+{
+    if ((transa != 'N') && (transa != 'n')) {
+	printf("unsupported value of 'transa'\n");
+    	return;
+    }
+
+    if ((transb != 'N') && (transb != 'n')) {
+	printf("unsupported value of 'transb'\n");
+	return;
+    }
+
+    if ((alpha - 1.0f > 1e-10) || (alpha - 1.0f < -1e-10)) {
+	printf("unsupported value of alpha\n");
+	return;
+    }
+
+    if ((beta - 0.0f > 1e-10) || (beta - 0.0f < -1e-10)) {
+	printf("unsupported value of beta\n");
+	return;
+    }
+
+    // Initialize thread block and kernel grid dimensions ---------------------
+
+
+    //INSERT CODE HERE
+
+    // Invoke CUDA kernel -----------------------------------------------------
+
+    //INSERT CODE HERE
+
+
+
+}
---
+++ lab3-sgemm-tiled/main.cu
@@ -0,0 +1,130 @@
+/******************************************************************************
+ *cr
+ *cr            (C) Copyright 2010 The Board of Trustees of the
+ *cr                        University of Illinois
+ *cr                         All Rights Reserved
+ *cr
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "kernel.cu"
+#include "support.h"
+
+int main (int argc, char *argv[])
+{
+
+    Timer timer;
+    cudaError_t cuda_ret;
+
+    // Initialize host variables ----------------------------------------------
+
+    printf("\nSetting up the problem..."); fflush(stdout);
+    startTime(&timer);
+
+    float *A_h, *B_h, *C_h;
+    float *A_d, *B_d, *C_d;
+    size_t A_sz, B_sz, C_sz;
+    unsigned matArow, matAcol;
+    unsigned matBrow, matBcol;
+    dim3 dim_grid, dim_block;
+
+    if (argc == 1) {
+        matArow = 1000;
+        matAcol = matBrow = 1000;
+        matBcol = 1000;
+    } else if (argc == 2) {
+        matArow = atoi(argv[1]);
+        matAcol = matBrow = atoi(argv[1]);
+        matBcol = atoi(argv[1]);
+    } else if (argc == 4) {
+        matArow = atoi(argv[1]);
+        matAcol = matBrow = atoi(argv[2]);
+        matBcol = atoi(argv[3]);
+    } else {
+        printf("\n    Invalid input parameters!"
+      "\n    Usage: ./sgemm-tiled                # All matrices are 1000 x 1000"
+      "\n    Usage: ./sgemm-tiled <m>            # All matrices are m x m"
+      "\n    Usage: ./sgemm-tiled <m> <k> <n>    # A: m x k, B: k x n, C: m x n"
+      "\n");
+        exit(0);
+    }
+
+    A_sz = matArow*matAcol;
+    B_sz = matBrow*matBcol;
+    C_sz = matArow*matBcol;
+
+    A_h = (float*) malloc( sizeof(float)*A_sz );
+    for (unsigned int i=0; i < A_sz; i++) { A_h[i] = (rand()%100)/100.00; }
+
+    B_h = (float*) malloc( sizeof(float)*B_sz );
+    for (unsigned int i=0; i < B_sz; i++) { B_h[i] = (rand()%100)/100.00; }
+
+    C_h = (float*) malloc( sizeof(float)*C_sz );
+
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+    printf("    A: %u x %u\n    B: %u x %u\n    C: %u x %u\n", matArow, matAcol,
+        matBrow, matBcol, matArow, matBcol);
+
+    // Allocate device variables ----------------------------------------------
+
+    printf("Allocating device variables..."); fflush(stdout);
+    startTime(&timer);
+
+    //INSERT CODE HERE
+
+
+    cudaDeviceSynchronize();
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    // Copy host variables to device ------------------------------------------
+
+    printf("Copying data from host to device..."); fflush(stdout);
+    startTime(&timer);
+
+    //INSERT CODE HERE
+
+
+
+    cudaDeviceSynchronize();
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    // Launch kernel using standard sgemm interface ---------------------------
+    printf("Launching kernel..."); fflush(stdout);
+    startTime(&timer);
+    basicSgemm('N', 'N', matArow, matBcol, matBrow, 1.0f, \
+		A_d, matArow, B_d, matBrow, 0.0f, C_d, matBrow);
+
+    cuda_ret = cudaDeviceSynchronize();
+	if(cuda_ret != cudaSuccess) FATAL("Unable to launch kernel");
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    // Copy device variables from host ----------------------------------------
+
+    printf("Copying data from device to host..."); fflush(stdout);
+    startTime(&timer);
+
+    //INSERT CODE HERE
+
+
+    cudaDeviceSynchronize();
+    stopTime(&timer); printf("%f s\n", elapsedTime(timer));
+
+    // Verify correctness -----------------------------------------------------
+
+    printf("Verifying results..."); fflush(stdout);
+
+    verify(A_h, B_h, C_h, matArow, matAcol, matBcol);
+
+
+    // Free memory ------------------------------------------------------------
+
+    free(A_h);
+    free(B_h);
+    free(C_h);
+
+    //INSERT CODE HERE
+
+    return 0;
+
+}
